b68b3e576449c334d84010e0de027d5e3fcfac5a
aamaiyaa <74420296+aamaiyaa@users.noreply.github.com>
2023-12-08 14:30:52 -0500

    bookmarkS

--- users/schema.js
+++ users/schema.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    id: Number,
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    followers: { type: [Number], default: [] },
-    following: { type: [Number], default: [] },
-    bio: { type:String, default: ""},
-    profilePicture: { type: String, default: ""},
-    userType: { type: String, enum: ["DIRECTOR", "TYPICAL USER"]},
-    email: { type: String, required: true, unique: true },
-    phoneNumber: { type:String, default: ""},
-    address: { type:String, default: ""},
-    bookmark: { type: [Number], default: []},
-  },
-  { collection: "users" });
-  
+const userSchema = new mongoose.Schema(
+ {
+  name: { type: String, required: true },
+  id: Number,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  followers: { type: [Number], default: [] },
+  following: { type: [Number], default: [] },
+  bio: { type: String, default: "" },
+  profilePicture: { type: String, default: "" },
+  userType: { type: String, enum: ["DIRECTOR", "TYPICAL USER"] },
+  email: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, default: "" },
+  address: { type: String, default: "" },
+  bookmarks: { type: [Number], default: [] },
+ },
+ { collection: "users" }
+);
+
 export default userSchema;
