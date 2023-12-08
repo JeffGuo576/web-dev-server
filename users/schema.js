8e4ffc56971e0a5fb7c52e7bbfe0ee53225777ef
Alexander Kouyoumjian <123589862+AKouyoumjian@users.noreply.github.com>
2023-12-08 16:00:06 -0500

    fixing updateUser

--- users/schema.js
+++ users/schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    id: Number,
+    id: String,
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     followers: { type: [Number], default: [] },
