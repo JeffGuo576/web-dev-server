8416a0db0c3c3a6df558427efc2369d1c087e441
aamaiyaa <74420296+aamaiyaa@users.noreply.github.com>
2023-12-09 14:52:31 -0500

    update user

--- users/dao.js
+++ users/dao.js
@@ -1,9 +1,12 @@
-import model from "./model.js";   
+import model from "./model.js";
 export const createUser = (user) => model.create(user);
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
-export const findUserByUsername = (username) =>  model.findOne({ username: username });
-export const findUserByCredentials = (username, password) => model.findOne({ username, password });
-export const updateUser = (userId, user) => model.updateOne({ id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ id: userId });
+export const findUserByUsername = (username) =>
+ model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>
+ model.findOne({ username, password });
+export const updateUser = (userId, user) =>
+ model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 export const totalUsers = () => model.countDocuments();
--- users/schema.js
+++ users/schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
  {
   name: { type: String, required: true },
-  id: String,
+  id: { type: String },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   followers: { type: [String], default: [] },
